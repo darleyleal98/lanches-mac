ec8429537e9bce12d20190290aa09242dff8020d
Darley Leal <darleyleal98@gmail.com>
2023-11-22 04:29:01 -0300

    Adicionado as anotions a classe categoria

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,10 +1,27 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "O nome do lanche deve ser informado!")]
+        [Display(Name = "Nome do lanche")]
         public string  Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no mínimo {1} caracter")]
+        [MaxLength(200, ErrorMessage = "Descrição deve ter no máximo {200} caracteres")]
         public string  DescricaoCurta { get; set; }
+
+        [NotMapped]
+        public DateTime DataDeCriacao { get; set; }
         public string  DescricaoDetalhada { get; set; }
         public decimal  Preco { get; set; }
         public string  ImagemUrl { get; set; }
