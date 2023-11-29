e5af6585759cd885ab515a819c86facd7e24492f
Darley Leal <darleyleal98@gmail.com>
2023-11-29 04:49:01 -0300

    Alterações foram feitas

---
+++ LanchesMac/Controllers/BebidaController.cs
@@ -0,0 +1,25 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class BebidaController : Controller
+    {
+        private readonly IBebidaRepository _bebidaRepository;
+
+        public BebidaController(IBebidaRepository bebidaRepository)
+        {
+            _bebidaRepository = bebidaRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var bebidasListViewModels = new BebidaListViewModel();
+            bebidasListViewModels.Bebidas = bebidasListViewModels.Bebidas;
+            bebidasListViewModels.CategoriaAtual = "Bebidas";
+
+            return View(bebidasListViewModels);
+        }
+    }
+}
